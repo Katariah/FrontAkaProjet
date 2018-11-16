2484bd28f9557541237209dfaf785e95e5db7e87
Fanny Silencieux <fanny.silencieux@gmail.com>
2018-11-16 12:11:36 +0100

    Commit de la partie Angular du projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { CarteComponent } from './carte/carte.component';
+import { CommanderComponent } from './commander/commander.component';
+import { CompteComponent } from './compte/compte.component';
+import { ContacterComponent } from './contacter/contacter.component';
+import { LoginComponent } from './login/login.component';
+import { NotificationComponent } from './notification/notification.component';
+import { SuggestionComponent } from './suggestion/suggestion.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'accueil', component: AccueilComponent},
+{path: 'carte', component: CarteComponent},
+{path: 'commander', component: CommanderComponent},
+{path: 'compte', component: CompteComponent},
+{path: 'contacter', component: ContacterComponent},
+{path: 'login', component: LoginComponent},
+{path: 'notification', component: NotificationComponent},
+{path: 'suggestion', component: SuggestionComponent},
+{path: '', pathMatch: 'full' , redirectTo: '/accueil'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AccueilComponent } from './accueil/accueil.component';
+import { CarteComponent } from './carte/carte.component';
+import { CommanderComponent } from './commander/commander.component';
+import { ContacterComponent } from './contacter/contacter.component';
+import { CompteComponent } from './compte/compte.component';
+import { LoginComponent } from './login/login.component';
+import { NotificationComponent } from './notification/notification.component';
+import { SuggestionComponent } from './suggestion/suggestion.component';
+import {MatCardModule} from '@angular/material/card';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    CarteComponent,
+    CommanderComponent,
+    ContacterComponent,
+    CompteComponent,
+    LoginComponent,
+    NotificationComponent,
+    SuggestionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
